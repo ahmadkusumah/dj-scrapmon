3e804e2d883d70deb3dd9c4d046080413a8b2b09
Aah Ahmad Kusumah <ahmad.kusumah@gmail.com>
2018-02-26 13:59:27 +0700

    print data

--- scrapmon/models.py
+++ scrapmon/models.py
@@ -47,7 +47,7 @@ class ScrapyerBatch(models.Model):
     enviroment = models.CharField(max_length=250, default='staging', null=True)
     run_script = models.NullBooleanField(default=None)
 
-    def __str__():
+    def __str__(self):
         return self.batch_name
 
 class ScrapyerBatchScript(models.Model):
@@ -93,44 +93,44 @@ def scrapy_log_saved(sender, instance, created, **kwargs):
         t.start()
 
 @receiver(post_save, sender=ScrapyerBatch)
-def scrapyer_batch_saved(sender, script, created, **kwargs):
+def scrapyer_batch_saved(sender, instance, created, **kwargs):
 
     def __runjob(script):
         ## retrieve script
-        for batch_script in ScrapyerBatchScript.objects.filter(batch=instance).order_by('order'):
+        for batch_script in ScrapyerBatchScript.objects.filter(batch=script).order_by('order'):
             instance = batch_script.script
             log = ScrapyLog(
                 start = timezone.now(),
-                script = script,
+                script = instance,
                 running = True,
-                scrapylog_name = script.spider_name+"_"+str(timezone.now().strftime('%Y%m'))
+                scrapylog_name = instance.spider_name+"_"+str(timezone.now().strftime('%Y%m'))
                 )
             log.save()
 
             command = ''
-            if batch_script.sites_new is None or not batch_script.sites_new.strip() :
-                command = '{venv} && cd {dir} && SCRAPYER_ENV={env} scrapy crawl {spider_name} -a recreate={recreate} -a start_date={start_date} -a end_date={end_date} -t csv --loglevel=INFO --logfile=/var/apps/a/data-scrapyer/shared/log/{logfile}.log'.format(env=batch_script.enviroment, dir=instance.project_dir, spider_name=instance.spider_name, recreate=instance.recreate, start_date=instance.start.strftime('%Y-%m-%d'), end_date=instance.end.strftime('%Y-%m-%d'), venv=instance.virtualenv, logfile=instance.spider_name+instance.start.strftime('%Y%m'))
+            if script.sites_new is None or not script.sites_new.strip() :
+                command = '{venv} && cd {dir} && SCRAPYER_ENV={env} scrapy crawl {spider_name} -a recreate={recreate} -a start_date={start_date} -a end_date={end_date} -t csv --loglevel=INFO --logfile=/var/apps/a/data-scrapyer/shared/log/{logfile}.log'.format(env=script.enviroment, dir=instance.project_dir, spider_name=instance.spider_name, recreate=instance.recreate, start_date=script.start.strftime('%Y-%m-%d'), end_date=script.end.strftime('%Y-%m-%d'), venv=instance.virtualenv, logfile=instance.spider_name+script.start.strftime('%Y%m'))
             else:
-                command = '{venv} && cd {dir} && SCRAPYER_ENV={env} scrapy crawl {spider_name} -a recreate={recreate} -a sites_new={sites_new} -a start_date={start_date} -a end_date={end_date} -t csv --loglevel=INFO --logfile=/var/apps/a/data-scrapyer/shared/log/{logfile}.log '.format(env=batch_script.enviroment, dir=instance.project_dir, spider_name=instance.spider_name, sites_new=batch_script.sites_new, recreate=instance.recreate, start_date=instance.start.strftime('%Y-%m-%d'), end_date=instance.end.strftime('%Y-%m-%d'), venv=instance.virtualenv, logfile=instance.spider_name+instance.start.strftime('%Y%m'))
+                command = '{venv} && cd {dir} && SCRAPYER_ENV={env} scrapy crawl {spider_name} -a recreate={recreate} -a sites_new={sites_new} -a start_date={start_date} -a end_date={end_date} -t csv --loglevel=INFO --logfile=/var/apps/a/data-scrapyer/shared/log/{logfile}.log '.format(env=script.enviroment, dir=instance.project_dir, spider_name=instance.spider_name, sites_new=script.sites_new, recreate=instance.recreate, start_date=script.start.strftime('%Y-%m-%d'), end_date=script.end.strftime('%Y-%m-%d'), venv=instance.virtualenv, logfile=instance.spider_name+script.start.strftime('%Y%m'))
 
             print('=============================')
             print(command)
             print('=============================')
-            # data = subprocess.run(command, shell=True, check=False, stderr=PIPE, stdout=PIPE, executable='/bin/bash')
+            data = subprocess.run(command, shell=True, check=False, stderr=PIPE, stdout=PIPE, executable='/bin/bash')
 
-            # if data.returncode == 0:
-            #     log.success = True
-            #     log.running = False
-            #     log.traceback = data.stderr.splitlines()[-103:]+data.stdout.splitlines()[-23:]
-            # else:
-            #     log.success = False
-            #     log.running = False
-            #     log.error_message = data.stderr.splitlines()[-50:]
-            #     log.traceback = data.stdout.splitlines()[-23:]
-            # log.save()
+            if data.returncode == 0:
+                log.success = True
+                log.running = False
+                log.traceback = data.stderr.splitlines()[-103:]+data.stdout.splitlines()[-23:]
+            else:
+                log.success = False
+                log.running = False
+                log.error_message = data.stderr.splitlines()[-50:]
+                log.traceback = data.stdout.splitlines()[-23:]
+            log.save()
 
-    if script.run_script:
-        t = Thread(target=__runjob, args=(script), daemon=True)
+    if instance.run_script:
+        t = Thread(target=__runjob, args=(instance,), daemon=True)
         t.start()
 
 '''This for set environemnt as options select'''
